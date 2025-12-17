b7f442af5f139c7cf28da3202156d14f6050ef2a
SH20RAJ <shaswatraj3@gmail.com>
2025-12-17 16:24:22 +0530

    feat(backend): initialize server and database connection

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import { connectToDatabase } from './db/db.js';
+import authRouter from './routes/auth.js'
+import departmentRouter from './routes/department.js'
+import employeeRouter from './routes/employee.js'
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectToDatabase();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api/auth', authRouter)
+app.use('/api/department', departmentRouter)
+app.use('/api/employee', employeeRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
